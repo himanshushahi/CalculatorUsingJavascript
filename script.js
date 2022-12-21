149834185b15689c043361f991ad93f1844b20dd
Himanshu Kumar Shahi <113952620+himanshushahi@users.noreply.github.com>
2022-12-21 21:06:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+//         document.getElementById("nine").addEventListener("click",insert(9));
+//         document.getElementById("eight").addEventListener("click",insert(8));;
+//         document.getElementById("seven").addEventListener("click",insert(7));
+//         document.getElementById("plus").addEventListener("click",insert("+"));
+//         document.getElementById("six").addEventListener("click",insert(6));
+//         document.getElementById("five").addEventListener("click", insert(5));
+//         document.getElementById("four").addEventListener("click",insert(4));
+//         document.getElementById("minus").addEventListener("click",insert("-"));
+//         document.getElementById("three").addEventListener("click",insert(3));
+//         document.getElementById("two").addEventListener("click",insert(2));
+//         document.getElementById("one").addEventListener("click",insert(1));
+//         document.getElementById("multiple").addEventListener("click",insert("*"));
+//         document.getElementById("zero").addEventListener("click",insert(0));
+//         document.getElementById("point").addEventListener("click",insert("."));
+//         document.getElementById("divide").addEventListener("click",insert ("/"));
+//         // document.getElementById("clear").addEventListener("click",insert());
+//         // document.getElementById("equal").addEventListener("click",insert("="));
+//         // document.getElementById("backspace").addEventListener("click",backspace());
+
+   
